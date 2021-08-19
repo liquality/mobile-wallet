ae9f2b55c424e6f6b7687c542097346e2de1d2ed
Adil Mezghouti <adilmezghouti@gmail.com>
2021-08-19 13:23:20 -0400

    set up lint + prettier + husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-};
+  rules: {
+    semi: 0,
+    'no-console': 2,
+    'array-bracket-spacing': 2,
+    quotes: 2,
+    'comma-dangle': 2,
+    'arrow-parens': 2,
+  },
+}
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 /**
  * @format
  */
-
 import './shim.js'
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native'
 // require('node-libs-react-native/globals');
-import App from './App';
-import {name as appName} from './app.json';
-AppRegistry.registerComponent(appName, () => App);
+import App from './App'
+import { name as appName } from './app.json'
+AppRegistry.registerComponent(appName, () => App)
