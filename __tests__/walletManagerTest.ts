e1dd8e97a204f91fab2af547dff7e6495523da4c
Adil Mezghouti <adilmezghouti@gmail.com>
2021-09-01 12:39:33 -0400

    started adding wallet functionality + unit tests

---
+++ __tests__/walletManagerTest.ts
@@ -0,0 +1,25 @@
+import WalletManager from '../src/core/walletManager'
+
+describe('WalletManagerTest', () => {
+  const PASSWORD = 'this is my cool password'
+  beforeAll(() => {
+    global.crypto = {}
+    global.crypto.getRandomValues = () => new Uint8Array(1)
+  })
+
+  it('should create a wallet', async () => {
+    const wallet = {
+      id: '1234',
+      at: Date.now(),
+      name: 'Account-1',
+      mnemomnic: 'anjsnc8383jndndj',
+      imported: false,
+    }
+    const walletManager = new WalletManager(wallet, PASSWORD)
+    const newWallet = await walletManager.createWallet()
+    expect(newWallet.keySalt).toBeTruthy()
+    expect(newWallet.wallets).toBeTruthy()
+    expect(newWallet.encryptedWallets).toBeTruthy()
+    expect(newWallet.account).toBeTruthy()
+  })
+})
--- src/core/walletManager.ts
+++ src/core/walletManager.ts
@@ -1,9 +1,111 @@
+import { chains, assets } from '@liquality/cryptoassets'
+import { v4 as uuidv4 } from 'uuid'
+import EncryptionManager from './encryptionManager'
+import config, {
+  accountColors,
+  chainDefaultColors,
+  NetworkEnum,
+} from './config'
+import {
+  AccountType,
+  AccountWrapperType,
+  NetworkWrapperType,
+  WalletType,
+} from './types'
+
+// SETUP_WALLET (state, { key }) {
+//   state.key = key
+//   state.keyUpdatedAt = Date.now()
+//   state.setupAt = Date.now()
+// },
+
+// ACCEPT_TNC (state) {
+//   state.termsAcceptedAt = Date.now()
+// },
+
 class WalletManager {
-  constructor() {}
+  wallets: Array<WalletType>
+  password: string
+  cryptoassets: any
+  chains: any
 
-  public createWallet() {}
+  constructor(wallet: WalletType, password: string) {
+    this.wallets = [wallet]
+    this.password = password
+    this.cryptoassets = assets
+    this.chains = chains
+  }
 
-  public backupWallet() {}
+  /**
+   * Creates a wallet along with an account
+   */
+  public async createWallet() {
+    const encryptionManager = new EncryptionManager(this.password)
+    const account: AccountWrapperType = {}
+    const id = uuidv4()
+    const at = Date.now()
+    const { networks, defaultAssets } = config
+    const { encrypted: encryptedWallets, keySalt } =
+      await encryptionManager.encrypt(JSON.stringify(this.wallets))
+
+    networks.forEach((network: NetworkEnum) => {
+      const assetKeys = defaultAssets[network]
+      config.chains.forEach(async (chainId) => {
+        const assetList = assetKeys.filter((asset) => {
+          return this.cryptoassets[asset]?.chain === chainId
+        })
+
+        const chain = this.chains[chainId]
+        account[id] = {
+          [network]: {
+            id: uuidv4(),
+            name: `${chain.name} 1`,
+            chain: chainId,
+            type: 'default',
+            index: 0,
+            addresses: [],
+            assets: assetList,
+            balances: {},
+            color: this.getNextAccountColor(chainId, 0),
+            createdAt: at,
+            updatedAt: at,
+          } as AccountType,
+        } as NetworkWrapperType
+      })
+    })
+
+    return {
+      activeWalletId: id,
+      encryptedWallets,
+      keySalt,
+      wallets: this.wallets,
+      account,
+    }
+  }
+
+  /**
+   * Stores the encrypted wallets locally
+   */
+  public async backupWallet() {
+    // const encryptionManager = new EncryptionManager(this.password)
+    // const { encrypted, keySalt } = await encryptionManager.encrypt(
+    //   JSON.stringify(this.wallets),
+    // )
+    // store
+  }
+
+  private getNextAccountColor(chain: string, index: number) {
+    const defaultColor = chainDefaultColors[chain]
+    const defaultIndex = accountColors.findIndex((c) => c === defaultColor)
+    if (defaultIndex === -1) {
+      return defaultColor
+    }
+    const finalIndex = index + defaultIndex
+    if (finalIndex >= accountColors.length) {
+      return accountColors[defaultIndex]
+    }
+    return accountColors[finalIndex]
+  }
 }
 
 export default WalletManager
