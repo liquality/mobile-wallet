529b8041166e8a2d6e95a46b2cbf42c45d428cfd
Adil Mezghouti <adilmezghouti@gmail.com>
2021-10-05 14:17:42 -0400

    Styled the asset list

--- src/screens/wallet-features/overviewScreen.tsx
+++ src/screens/wallet-features/overviewScreen.tsx
@@ -30,91 +30,6 @@ const getAssetIcon = (asset: string) => {
   }
 }
 
-const renderAsset = ({ item }: { item: DataElementType }) => {
-  const { name, address, balance, balanceInUSD } = item
-  const isNested = item.assets && item.assets.length > 0
-
-  return (
-    <View>
-      <View
-        style={[
-          styles.row,
-          { borderLeftColor: item.color, borderLeftWidth: 3 },
-        ]}>
-        <View style={styles.col1}>
-          <Text style={styles.plusSign}>{isNested ? '+' : ''}</Text>
-          {getAssetIcon(item.name)}
-        </View>
-        <View style={styles.col2}>
-          <Text style={styles.name}>{name}</Text>
-          <Text style={styles.address}>
-            {`${address?.substring(0, 4)}...${address?.substring(
-              address?.length - 4,
-            )}`}
-          </Text>
-        </View>
-        {!isNested && (
-          <View style={styles.col3}>
-            <Text style={styles.balance}>{balance}</Text>
-            <Text style={styles.balanceInUSD}>{balanceInUSD}</Text>
-          </View>
-        )}
-        {!isNested && (
-          <View style={styles.col4}>
-            <Pressable>
-              <FontAwesomeIcon
-                size={20}
-                icon={faGreaterThan}
-                color={'#A8AEB7'}
-              />
-            </Pressable>
-          </View>
-        )}
-        {isNested && (
-          <View style={styles.col3}>
-            <Text style={styles.TotalBalanceInUSD}>
-              Total {balanceInUSD} USD
-            </Text>
-            <View style={styles.gas}>
-              <FontAwesomeIcon
-                size={20}
-                icon={faTachometerAlt}
-                color={'#9D4DFA'}
-                style={styles.gasIcon}
-              />
-              <Text style={styles.gasLabel}>Gas</Text>
-            </View>
-          </View>
-        )}
-      </View>
-      {isNested &&
-        item.assets!.map((subElem) => {
-          return (
-            <View style={[styles.row, styles.subElement]}>
-              <View style={styles.col1}>{getAssetIcon(subElem.name)}</View>
-              <View style={styles.col2}>
-                <Text style={styles.name}>{subElem.name}</Text>
-              </View>
-              <View style={styles.col3}>
-                <Text style={styles.balance}>{subElem.balance}</Text>
-                <Text style={styles.balanceInUSD}>{subElem.balanceInUSD}</Text>
-              </View>
-              <View style={styles.col4}>
-                <Pressable>
-                  <FontAwesomeIcon
-                    size={20}
-                    icon={faGreaterThan}
-                    color={'#A8AEB7'}
-                  />
-                </Pressable>
-              </View>
-            </View>
-          )
-        })}
-    </View>
-  )
-}
-
 type DataElementType = {
   id: string
   name: string
@@ -123,6 +38,7 @@ type DataElementType = {
   balanceInUSD: number
   color?: string
   assets?: Array<DataElementType>
+  showAssets?: boolean
 }
 
 const OverviewScreen = () => {
@@ -145,6 +61,113 @@ const OverviewScreen = () => {
     }),
   )
 
+  const toggleRow = (itemId: string) => {
+    setData(
+      data.map((item) => {
+        if (item.id === itemId) {
+          return {
+            ...item,
+            showAssets: !item.showAssets,
+          }
+        } else {
+          return item
+        }
+      }),
+    )
+  }
+
+  const renderAsset = ({ item }: { item: DataElementType }) => {
+    const { name, address, balance, balanceInUSD } = item
+    const isNested = item.assets && item.assets.length > 0
+
+    return (
+      <View>
+        <View
+          style={[
+            styles.row,
+            { borderLeftColor: item.color, borderLeftWidth: 3 },
+          ]}>
+          <View style={styles.col1}>
+            <Pressable onPress={() => toggleRow(item.id)}>
+              <Text style={styles.plusSign}>
+                {isNested ? (item.showAssets ? '-' : '+') : ''}
+              </Text>
+            </Pressable>
+            {getAssetIcon(item.name)}
+          </View>
+          <View style={styles.col2}>
+            <Text style={styles.name}>{name}</Text>
+            <Text style={styles.address}>
+              {`${address?.substring(0, 4)}...${address?.substring(
+                address?.length - 4,
+              )}`}
+            </Text>
+          </View>
+          {!isNested && (
+            <View style={styles.col3}>
+              <Text style={styles.balance}>{balance}</Text>
+              <Text style={styles.balanceInUSD}>{balanceInUSD}</Text>
+            </View>
+          )}
+          {!isNested && (
+            <View style={styles.col4}>
+              <Pressable>
+                <FontAwesomeIcon
+                  size={20}
+                  icon={faGreaterThan}
+                  color={'#A8AEB7'}
+                />
+              </Pressable>
+            </View>
+          )}
+          {isNested && (
+            <View style={styles.col3}>
+              <Text style={styles.TotalBalanceInUSD}>
+                Total {balanceInUSD} USD
+              </Text>
+              <View style={styles.gas}>
+                <FontAwesomeIcon
+                  size={20}
+                  icon={faTachometerAlt}
+                  color={'#9D4DFA'}
+                  style={styles.gasIcon}
+                />
+                <Text style={styles.gasLabel}>Gas</Text>
+              </View>
+            </View>
+          )}
+        </View>
+        {isNested &&
+          item.showAssets &&
+          item.assets!.map((subElem) => {
+            return (
+              <View style={[styles.row, styles.subElement]}>
+                <View style={styles.col1}>{getAssetIcon(subElem.name)}</View>
+                <View style={styles.col2}>
+                  <Text style={styles.name}>{subElem.name}</Text>
+                </View>
+                <View style={styles.col3}>
+                  <Text style={styles.balance}>{subElem.balance}</Text>
+                  <Text style={styles.balanceInUSD}>
+                    {subElem.balanceInUSD}
+                  </Text>
+                </View>
+                <View style={styles.col4}>
+                  <Pressable>
+                    <FontAwesomeIcon
+                      size={20}
+                      icon={faGreaterThan}
+                      color={'#A8AEB7'}
+                    />
+                  </Pressable>
+                </View>
+              </View>
+            )
+          })}
+      </View>
+    )
+  }
+
   useEffect(() => {
     const accts = accounts?.[walletId!]?.[activeNetwork!]
     if (accts && fiatRates) {
@@ -165,6 +188,7 @@ const OverviewScreen = () => {
           balanceInUSD: 0,
           color: account.color,
           assets: [],
+          showAssets: false,
         }
         const { total, assetsData } = Object.keys(account.balances!).reduce(
           (
@@ -398,7 +422,7 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end',
   },
   plusSign: {
-    marginRight: 20,
+    marginRight: 5,
   },
   subElement: {
     paddingLeft: 50,
