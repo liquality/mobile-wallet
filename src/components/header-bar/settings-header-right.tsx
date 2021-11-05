397428f1afa604c87effcdeee896aa9170a4cff4
Adil Mezghouti <adilmezghouti@gmail.com>
2021-11-05 17:14:30 -0400

    Addressed PR feedback

--- src/components/header-bar/settings-header-right.tsx
+++ src/components/header-bar/settings-header-right.tsx
@@ -3,15 +3,13 @@ import { faSignOut } from '@fortawesome/pro-light-svg-icons'
 import { Pressable, StyleSheet } from 'react-native'
 import React from 'react'
 
-const SettingsHeaderRight = ({
-  navigate,
-}: {
+const SettingsHeaderRight: React.FC<{
   navigate: (screen: string) => void
-}) => {
+}> = (props) => {
   return (
     <Pressable
       style={styles.signOutBtn}
-      onPress={() => navigate('LoginScreen')}>
+      onPress={() => props.navigate('LoginScreen')}>
       <FontAwesomeIcon
         icon={faSignOut}
         size={20}
