e16dd523dbd4474314828257bbca7920705f1e5a
Urvesh Ghoderao <urvesh109@Urveshs-MacBook-Pro.local>
2022-07-25 11:55:19 +0530

    For android react-native upgraded to 0.68.2

--- android/app/src/main/java/io/liquality/MainActivity.java
+++ android/app/src/main/java/io/liquality/MainActivity.java
@@ -1,6 +1,8 @@
 package io.liquality;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
 import org.devio.rn.splashscreen.SplashScreen;
 import android.content.Intent;
 
@@ -15,14 +17,26 @@ public class MainActivity extends ReactActivity {
     return "LiqualityMobile";
   }
 
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+   * you can specify the rendered you wish to use (Fabric or the older renderer).
+   */
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this);
-    super.onCreate(savedInstanceState);
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegateWrapper(this, new MainActivityDelegate(this, getMainComponentName()));
   }
 
-  @Override
-  public void onNewIntent(Intent intent) {
-    super.onNewIntent(intent);
+  public static class MainActivityDelegate extends ReactActivityDelegate {
+    public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
+      super(activity, mainComponentName);
+    }
+
+    @Override
+    protected ReactRootView createRootView() {
+      ReactRootView reactRootView = new ReactRootView(getContext());
+      // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+      reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
+      return reactRootView;
+    }
   }
 }
