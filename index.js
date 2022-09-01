89c05806038be452074382c18e1a4ec8a1184541
Urvesh109 <urveshi.t@gmail.com>
2022-09-01 15:40:16 +0530

    RefreshControlComponent added

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ import 'react-native-gesture-handler'
 import { AppRegistry, LogBox } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
-LogBox.ignoreLogs(['Require cycle:'])
+LogBox.ignoreLogs(['Require cycle:', 'VirtualizedLists should never be nested'])
 
 AppRegistry.registerComponent(appName, () => App)
--- src/screens/wallet-features/home/overview-screen.tsx
+++ src/screens/wallet-features/home/overview-screen.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react'
-import { Dimensions, StyleSheet } from 'react-native'
+import {
+  Dimensions,
+  StyleSheet,
+  ScrollView,
+  RefreshControl,
+} from 'react-native'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../../../types'
 import ErrorBoundary from 'react-native-error-boundary'
@@ -16,32 +21,49 @@ export type OverviewProps = NativeStackScreenProps<
   'OverviewScreen'
 >
 
+const wait = (timeout: number) => {
+  return new Promise((resolve) => setTimeout(resolve, timeout))
+}
+
 const OverviewScreen = ({ navigation }: OverviewProps) => {
+  const [refreshing, setRefreshing] = React.useState(false)
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true)
+    wait(2000).then(() => setRefreshing(false))
+  }, [])
+
   return (
     <Box flex={1}>
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <HandleLockWalletAndBackgroundTasks />
-        <React.Suspense
-          fallback={
-            <Box style={styles.overviewBlock}>
-              <GradientBackground
-                width={Dimensions.get('screen').width}
-                height={225}
-              />
-              <Text variant="loading" tx="overviewScreen.load" />
-            </Box>
-          }>
-          <SummaryBlock navigation={navigation} />
-        </React.Suspense>
-        <React.Suspense
-          fallback={
-            <Box>
-              <Text variant="loading" tx="overviewScreen.load" />
-            </Box>
-          }>
-          <ContentBlock />
-        </React.Suspense>
-      </ErrorBoundary>
+      <ScrollView
+        contentContainerStyle={styles.contentContainerStyle}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <HandleLockWalletAndBackgroundTasks />
+          <React.Suspense
+            fallback={
+              <Box style={styles.overviewBlock}>
+                <GradientBackground
+                  width={Dimensions.get('screen').width}
+                  height={225}
+                />
+                <Text variant="loading" tx="overviewScreen.load" />
+              </Box>
+            }>
+            <SummaryBlock navigation={navigation} />
+          </React.Suspense>
+          <React.Suspense
+            fallback={
+              <Box>
+                <Text variant="loading" tx="overviewScreen.load" />
+              </Box>
+            }>
+            <ContentBlock />
+          </React.Suspense>
+        </ErrorBoundary>
+      </ScrollView>
     </Box>
   )
 }
@@ -52,6 +74,9 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 225,
     paddingVertical: 10,
+  },
+  contentContainerStyle: {
+    flex: 1,
   },
 })
 
