67b54441617a2e18b5df2ddca0e9edb196cfae3a
Urvesh109 <urveshi.t@gmail.com>
2022-09-22 22:49:00 +0530

    fonts usage updates

--- src/assets/fonts.ts
+++ src/assets/fonts.ts
@@ -1,13 +1,7 @@
 export default {
   Bold: 'AnekKannada-Bold',
-  ExtraBold: 'AnekKannada-ExtraBold',
-  ExtraLight: 'AnekKannada-ExtraLight',
   Light: 'AnekKannada-Light',
-  Medium: 'AnekKannada-Medium',
   Regular: 'AnekKannada-Regular',
   SemiBold: 'AnekKannada-SemiBold',
   Thin: 'AnekKannada-Thin',
-  JetBrainsMonoBold: 'JetBrainsMono-Bold',
-  JetBrainsMonoMedium: 'JetBrainsMono-Medium',
-  JetBrainsMonoRegular: 'JetBrainsMono-Regular',
 }
