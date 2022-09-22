62027fab872fe5104fc48e58b81aee530b70028a
Urvesh109 <urveshi.t@gmail.com>
2022-09-22 15:29:02 +0530

    Facelift Palette added

---
+++ src/theme/faceliftPalette.ts
@@ -0,0 +1,17 @@
+export const faceliftPalette = {
+  white: '#ffffff',
+  lightWhite: '#F9F9F9',
+  mediumWhite: '#F4F4F4',
+  whiteGrey: '#EEEEEE',
+  lightGrey: '#E0E0E0',
+  mediumGrey: '#BFC3C7',
+  grey: '#A8AEB7',
+  greyMeta: '#646F85',
+  darkGrey: '#3D4767',
+  headerBackground: '#313747',
+  mainBackground: '#282E3F',
+  greyBlack: '#000D35',
+  buttonActive: '#7700D5',
+  buttonDefaul: '#9742FB',
+  buttonDisabled: '#BA62FE',
+}
