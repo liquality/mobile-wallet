14e7af1cf742a7a6af3e5bd5bf318656fe022eed
Urvesh109 <urveshi.t@gmail.com>
2022-10-05 23:06:06 +0530

    Temp commit

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -49,14 +49,14 @@ export const theme = createTheme({
   },
   spacing: {
     vs: scale(2),
-    s: scale(5),
+    s: scale(3),
     m: scale(10),
     l: scale(15),
     xl: scale(20),
     xxl: scale(50),
     xxxl: scale(70),
-    onboardingPadding: scale(40),
-    onboardingHeaderPadding: scale(25),
+    onboardingPadding: scale(36),
+    onboardingHeaderPadding: scale(21),
   },
   breakpoints: {
     phone: 0,
@@ -295,14 +295,14 @@ export const theme = createTheme({
     },
     termsBody: {
       fontFamily: Fonts.Regular,
-      fontSize: scale(17),
+      fontSize: scale(15),
       fontWeight: '400',
     },
     h1: {
       fontFamily: Fonts.Regular,
-      fontSize: scale(39),
+      fontSize: scale(37),
       fontWeight: '500',
-      lineHeight: scale(53),
+      lineHeight: scale(49),
     },
     h3: {
       fontFamily: Fonts.Regular,
